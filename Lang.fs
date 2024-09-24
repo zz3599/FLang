bc1d2d56b2fd274da76e0be3211750603200e6ac
Brook Zhou <brookzhou@microsoft.com>
2024-09-23 22:34:53 -0700

    basic evaluation

---
+++ Lang.fs
@@ -0,0 +1,111 @@
+﻿module Lang
+
+type VarName = string
+
+module VarName =
+    let make (name: string) = name
+
+[<RequireQualifiedAccess>]
+type BType =
+    | Int of int
+    | Unit
+
+type ArithmeticFn =
+    | Add
+    | Sub
+    | Mul
+    | Div
+
+module ArithmeticFn = 
+    let apply (fn: ArithmeticFn) (a: int) (b: int) : int = 
+        match fn with
+        | Add -> a + b
+        | Sub -> a - b
+        | Mul -> a * b
+        | Div -> a / b
+
+type UnaryArithmeticFn = | Neg
+
+type ComparisonFn =
+    | Less
+    | Equal
+    | Greater
+
+module ComparisonFn =
+    let apply(fn: ComparisonFn) (a: int) (b: int) : int = 
+        match fn with
+        | Less -> if a < b then 1 else 0
+        | Equal -> if a = b then 1 else 0
+        | Greater -> if a < b then 1 else 0
+
+type BuiltinFn =
+    | Arithmetic of fn: ArithmeticFn * opA: Expr * opB: Expr
+    | Comparison of fn: ComparisonFn * lhs: Expr * rhs: Expr
+
+and Expr =
+    | Lit of BType
+    | Var of VarName
+    // abstraction
+    | Abs of var: VarName * body: Expr
+    // application of a function to an argument
+    | App of expr: Expr * arg: Expr
+    | Cond of pred: Expr * trueBranch: Expr * falseBranch: Expr
+    //| Bind of recursive: bool * var: VarName * body: Expr * expr: Expr
+    | Builtin of BuiltinFn
+
+type EvalError = 
+    | Wrongtype of Expr * expectedType: string
+exception EvalException of EvalError
+
+module Expr = 
+    let asInt = function
+        | Lit(BType.Int n) -> n
+        | other -> raise (EvalException (Wrongtype(other, "int")))
+    let asAbs = function
+        | Abs(var, body) -> (var, body)
+        | other -> raise (EvalException (Wrongtype(other, "abs")))
+    let rec substitute (replaceWhat: VarName) (replaceFor: Expr) (expr: Expr) : Expr =
+        let substFn = substitute replaceWhat replaceFor
+        match expr with
+        | Lit _ -> expr
+        | Builtin(Arithmetic(fn, opA, opB)) -> 
+            Builtin(Arithmetic(fn, substFn opA, substFn opB))
+        | Builtin(Comparison(fn, opA, opB)) -> 
+            Builtin(Comparison(fn, substFn opA, substFn opB))
+        | Cond(pred, trueBranch, falseBranch) -> 
+            Cond(substFn pred, substFn trueBranch, substFn falseBranch)
+        | App(expr, arg) -> 
+            App(substFn expr, substFn arg)
+        | Abs(boundName, body) -> 
+            if boundName = replaceWhat then replaceFor else Abs(boundName, substFn body)
+        | Var boundName ->
+            if boundName = replaceWhat then replaceFor else expr      
+
+let rec eval (expr: Expr) : Expr = 
+    match expr with 
+    | Lit _ -> expr
+    | Builtin (Arithmetic( fn, opA, opB)) -> 
+        let valA = eval opA |> Expr.asInt
+        let valB = eval opB |> Expr.asInt
+        Lit (BType.Int(ArithmeticFn.apply fn valA valB))
+    | Builtin (Comparison(fn, lhs, rhs)) -> 
+        let valA = eval lhs |> Expr.asInt
+        let valB = eval rhs |> Expr.asInt
+        Lit (BType.Int(ComparisonFn.apply fn valA valB))     
+    | Cond(pred, trueBranch, falseBranch) -> 
+        let predVal = eval pred |> Expr.asInt
+        if predVal <> 0 then eval trueBranch else eval falseBranch
+    | Abs(var, body) -> expr
+    | App(expr, arg) -> 
+        let lambdaVar, lambdaBody = eval expr |> Expr.asAbs 
+        Expr.substitute lambdaVar arg lambdaBody |> eval
+    | Var name -> expr
+
+module Example = 
+    let lit (n: int) = Lit(BType.Int n)
+    let incrFn = 
+        Abs(
+            var = "x", 
+            body = Builtin(Arithmetic(Add, Var("x"), lit 1))
+        )
+    let incrApp (n: int) = App(expr = incrFn, arg = lit n)
---
+++ Program.fs
@@ -0,0 +1,114 @@
+﻿module HelloSquare
+
+/// Squares a value.
+let square x = x * x
+
+/// Adds 1 to a value.
+let addOne x = x + 1
+
+/// Tests if an integer value is odd via modulo.
+///
+/// '<>' is a binary comparison operator that means "not equal to".
+let isOdd x = x % 2 <> 0
+
+/// A list of 5 numbers.  More on lists later.
+let numbers = [ 1; 2; 3; 4; 5 ]
+
+/// Given a list of integers, it filters out the even numbers,
+/// squares the resulting odds, and adds 1 to the squared odds.
+let squareOddValuesAndAddOne values =
+    let odds = List.filter isOdd values
+    let squares = List.map square odds
+    let result = List.map addOne squares
+    List.map
+    result
+
+let swapElems (a, b) = (b, a)
+
+
+let rnd = System.Random()
+
+/// This is an infinite sequence which is a random walk.
+/// This example uses yield! to return each element of a subsequence.
+let rec randomWalk x =
+    seq { yield x
+          yield! randomWalk (x + rnd.NextDouble() - 0.5) }
+
+/// This example shows the first 100 elements of the random walk.
+let first100ValuesOfRandomWalk =
+    randomWalk 5.0
+    |> Seq.truncate 100
+    |> Seq.toList
+
+printfn $"First 100 elements of a random walk: {first100ValuesOfRandomWalk}"
+
+module DiscriminatedUnions =
+
+    /// The following represents the suit of a playing card.
+    type Suit =
+        | Hearts
+        | Clubs
+        | Diamonds
+        | Spades
+
+    /// A Discriminated Union can also be used to represent the rank of a playing card.
+    type Rank =
+        /// Represents the rank of cards 2 .. 10
+        | Value of int
+        | Ace
+        | King
+        | Queen
+        | Jack
+
+        /// Discriminated Unions can also implement object-oriented members.
+        static member GetAllRanks() =
+            [ yield Ace
+              for i in 2 .. 10 do yield Value i
+              yield Jack
+              yield Queen
+              yield King ]
+
+    /// This is a record type that combines a Suit and a Rank.
+    /// It's common to use both Records and Discriminated Unions when representing data.
+    type Card = { Suit: Suit; Rank: Rank }
+
+    /// This computes a list representing all the cards in the deck.
+    let fullDeck =
+        [ for suit in [ Hearts; Diamonds; Clubs; Spades] do
+              for rank in Rank.GetAllRanks() do
+                  yield { Suit=suit; Rank=rank } ]
+
+    /// This example converts a 'Card' object to a string.
+    let showPlayingCard (c: Card) =
+        let rankString =
+            match c.Rank with
+            | Ace -> "Ace"
+            | King -> "King"
+            | Queen -> "Queen"
+            | Jack -> "Jack"
+            | Value n -> string n
+        let suitString =
+            match c.Suit with
+            | Clubs -> "clubs"
+            | Diamonds -> "diamonds"
+            | Spades -> "spades"
+            | Hearts -> "hearts"
+        rankString  + " of " + suitString
+
+    /// This example prints all the cards in a playing deck.
+    let printAllCards() =
+        for card in fullDeck do
+            printfn $"{showPlayingCard card}"
+
+
+
+
+[<EntryPoint>]
+let main argv =
+    printfn "%d squared is: %d!" 12 (square 12)
+    let tuple = (1, 2)
+    printfn $"The result of swapping {tuple} is {(swapElems tuple)}"
+    printfn $"processing {numbers} through 'squareOddValuesAndAddOne' produces: {squareOddValuesAndAddOne numbers}"
+    DiscriminatedUnions.printAllCards()
+    printfn $"{Lang.eval (Lang.Example.incrApp 42)}"
+    0 // Return an integer exit code
